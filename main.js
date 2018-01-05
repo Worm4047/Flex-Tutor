596fd9d18640e3f4f453f49e8c8d55d091c6a529
Worm4047 <abhidagar@rocketmail.com>
2018-01-05 20:30:00 +0530

    All Done

--- main.js
+++ main.js
@@ -8,5 +8,30 @@ $(document).ready(function(){
 			console.log("Checked", category, prop_value);
 			parent_prop_div.css(category,prop_value);
 		}
+	});
+	var divs = ['red','blue','green','orange','purple'];
+	$(".prop-entered").on('keyup', function (e) {
+	    if (e.keyCode != 13) return;
+	    var elem = $(e.target);
+	    var prop_value = e.target.value;
+	    var category = elem.attr('category');
+	    var item_no = elem.attr('item');
+	    var prop_div = elem.parent().parent().next().find('.'+divs[item_no-1]); 
+	    console.log(elem,prop_value,category,item_no,prop_div);
+	    prop_div.css(category, prop_value);
+	});
+	$('.child-prop-selected').click(function(e){
+		var elem = $(e.target);
+	    var prop_value = e.target.value;
+	    
+
+	    var category = elem.attr('category');
+	    var prop_div = elem.parent().parent().next().find('.green'); 
+	    console.log(elem,prop_value,category,prop_div);
+	    if(prop_value === 'stretch')
+	    	prop_div.css('height','auto');
+	    else
+	    	prop_div.css('height','200px');
+	    prop_div.css(category, prop_value);
 	})
 })
